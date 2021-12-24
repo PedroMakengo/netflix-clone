e47cbcb86acbe11ddd058ba88f39eaef77461a44
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-12-24 13:38:20 -0300

    Terminado a primeira fase

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import React, { useEffect, useState } from 'react'
 import './App.css';
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
+import FeatureMovie from './components/FeatureMovie';
 
 export default () => {
+
   const [movieList, setMovieList] = useState([]);
-  console.log(movieList)
+  const [featuredData, setFeaturedData] = useState(null);
 
   // Pesquisar e procurar entender o useEffect
   useEffect(() => {
@@ -14,6 +16,13 @@ export default () => {
       // Pegando a lista Total 
       let list = await Tmdb.getHomeList();
       setMovieList(list);
+
+
+      // Pegar o Featured
+      let originals = list.filter(i => i.slug === 'originals'); // retornar um array
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+      console.log(chosen);
     }
 
     loadAll();
@@ -21,6 +30,11 @@ export default () => {
 
   return (
     <div className="page">
+
+     {featuredData && 
+      <FeatureMovie item={featuredData} />
+     }
+
      <section className="lists">
       {movieList.map((item, key) => (
         <MovieRow key={key} title={item.title} items={item.items} />
