a707a7d9afc45197caa585d8a7de211e1205b147
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-12-24 15:30:13 -0300

    Melhorando o banner

--- src/components/FeatureMovie.js
+++ src/components/FeatureMovie.js
@@ -3,7 +3,14 @@ import React from 'react';
 import './FeatureMovie.css';
 
 export default ({ item }) => {
-  console.log(item)
+  console.log(item);
+
+  let firsteDate = new Date(item.first_air_date);
+  let genres = [];
+  for(let i in item.genres) {
+    genres.push(item.genres[i].name)
+  }
+
   return (
     <section className="featured" style={{
       backgroundSize: 'cover',
@@ -15,7 +22,21 @@ export default ({ item }) => {
           <div className="featured--name">{item.original_name}</div>
           <div className="featured--info">
             <div className="featured--points">{item.vote_average} pontos</div>
-            <div className="featured--year">2099</div>
+            <div className="featured--year">{firsteDate.getFullYear()}</div>
+            <div className="featured--seasons">
+              {item.number_of_seasons} temporada
+              {item.number_of_seasons !== 1 ? 's': ''}
+            </div>
+            <div className="featured--description">
+              {item.overview}
+            </div>
+            <div className="featured--buttons">
+              <a href={`/watch/${item.id}`} className="featured--watchbutton">Assistir</a>
+              <a href={`/list/add/${item.id}`} className="featured--mylistbutton">+ Minha Lista</a>
+            </div>
+            <div className="featured--genres">
+              <strong>Gêneros:</strong> {genres.join(', ')} 
+            </div>
           </div>
         </div>
       </div>
