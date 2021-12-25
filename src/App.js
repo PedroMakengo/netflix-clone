d20c4a76378cafcfed6ee57ce8a42edc2e67ec4a
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-12-25 04:34:14 -0300

    Rodape feito com sucesso

--- src/App.js
+++ src/App.js
@@ -60,6 +60,13 @@ export default () => {
         <MovieRow key={key} title={item.title} items={item.items} />
       ))}
      </section>
+
+
+     <footer>
+       Feito com <span role="img" aria-label="coração">💖</span> pela B7Web <br />
+       Direitos de imagem para Netflix <br />
+       Dados pegos do site Themoviedb.org
+     </footer>
     </div>
   );
 }
